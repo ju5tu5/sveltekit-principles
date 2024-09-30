e43b32991e5780faf6c959c625e5353167a65793
Justus Sturkenboom <justus@ju5tu5.nl>
2024-09-30 11:17:00 +0200

    added some files so i don't have to type these live ;)

---
+++ src/routes/+page.server.js
@@ -0,0 +1,11 @@
+// import { PUBLIC_API_URL } from '$env/static/public'
+// import { API_KEY } from '$env/dynamic/private'
+// import fetchJson from '$lib/server/fetchJson'
+
+export async function load() {
+  // const url = PUBLIC_API_URL + 'person/'
+  // const persons = await fetchJson(url)
+  // return {
+  // persons: persons.data,
+  // }
+}
